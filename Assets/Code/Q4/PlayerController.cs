4f8901aa6756ab6278dbc1900f19b31d80c02fe0
Ruowen Xu <ruowen@wustl.edu>
2023-03-22 21:42:55 -0500

    Q6 complete

--- Assets/Code/Q4/PlayerController.cs
+++ Assets/Code/Q4/PlayerController.cs
@@ -10,6 +10,14 @@ namespace FPS
 
         public static PlayerController instance;
 
+        //Outlets
+        public Transform povOrigin;
+        public Transform projectileOrigin;
+        public GameObject projectilePrefab;
+
+        //Configuration
+        public float attackRange;
+
         public List<int> keyIDsObtained;
 
 
@@ -19,42 +27,86 @@ namespace FPS
             keyIDsObtained = new List<int>();
         }
 
-        // Update is called once per frame
-        void Update()
-        {
-            Keyboard keyboardInput = Keyboard.current;
-            Mouse mouseInput = Mouse.current;
-            if (keyboardInput != null && mouseInput != null)
-            {
-                // E KEY Interactions
-                if (keyboardInput.eKey.wasPressedThisFrame)
-                {
-                    Vector2 mousePosition = mouseInput.position.ReadValue();
-
-                    Ray ray = Camera.main.ScreenPointToRay(mousePosition);
-                    RaycastHit hit;
-                    if (Physics.Raycast(ray, out hit, 2f))
-                    {
-                        print("Interacted with" + hit.transform.name + "from" + hit.distance + "m.");
-
-                        Door targetDoor = hit.transform.GetComponent<Door>();
-                        if (targetDoor)
-                        {
-                            targetDoor.Interact();
-                        }
-
-                        InteractButton targetButton = hit.transform.GetComponent<InteractButton>();
-                        if(targetButton != null)
-                        {
-                            targetButton.Interact();
-                        }
-                    }
+        void OnPrimaryAttack() {
+            RaycastHit hit;
+            bool hitSomething = Physics.Raycast(povOrigin.position, povOrigin.forward, out hit, attackRange);
+            if (hitSomething) {
+                Rigidbody targetRigidbody = hit.transform.GetComponent<Rigidbody>();
+                if (targetRigidbody) {
+                    targetRigidbody.AddForce(povOrigin.forward * 100f, ForceMode.Impulse);
                 }
-
-            
-
-
             }
         }
+
+        // Update is called once per frame
+        //void Update()
+        //{
+        //    Keyboard keyboardInput = Keyboard.current;
+        //    Mouse mouseInput = Mouse.current;
+        //    if (keyboardInput != null && mouseInput != null)
+        //    {
+        //        // E KEY Interactions
+        //        if (keyboardInput.eKey.wasPressedThisFrame)
+        //        {
+        //            Vector2 mousePosition = mouseInput.position.ReadValue();
+
+        //            Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+        //            RaycastHit hit;
+        //            if (Physics.Raycast(ray, out hit, 2f))
+        //            {
+        //                print("Interacted with" + hit.transform.name + "from" + hit.distance + "m.");
+
+        //                Door targetDoor = hit.transform.GetComponent<Door>();
+        //                if (targetDoor)
+        //                {
+        //                    targetDoor.Interact();
+        //                }
+
+        //                InteractButton targetButton = hit.transform.GetComponent<InteractButton>();
+        //                if(targetButton != null)
+        //                {
+        //                    targetButton.Interact();
+        //                }
+        //            }
+        //        }
+        //        //Left Mouse Interaction
+        //        if (mouseInput.leftButton.wasPressedThisFrame) {
+        //            OnPrimaryAttack();
+        //        }
+
+        //        //Right Mouse Interactions
+        //        if (mouseInput.rightButton.wasPressedThisFrame) {
+        //            OnSecondaryAttack();
+        //        }
+        //    }
+        //}
+
+        void OnInteract()
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(povOrigin.position, povOrigin.forward, out hit, 2f))
+            {
+
+                Door targetDoor = hit.transform.GetComponent<Door>();
+                if (targetDoor)
+                {
+                    targetDoor.Interact();
+                }
+
+                InteractButton targetButton = hit.transform.GetComponent<InteractButton>();
+                if (targetButton != null)
+                {
+                    targetButton.Interact();
+                }
+            }
+        }
+
+        void OnSecondaryAttack()
+        {
+            GameObject projectile = Instantiate(projectilePrefab, projectileOrigin.position, Quaternion.LookRotation(povOrigin.forward));
+            projectile.transform.localScale = Vector3.one * 5f;
+            projectile.GetComponent<Rigidbody>().AddForce(povOrigin.forward * 25f, ForceMode.Impulse);
+        }
+
     }
 }
